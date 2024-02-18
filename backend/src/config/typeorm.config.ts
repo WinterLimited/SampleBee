da6e2b5944e4524c4733ad29ffd28e83372a9ca3
WOOIL, KIM <37162892+WinterLimited@users.noreply.github.com>
2024-02-19 03:08:46 +0900

    test: process.env load

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,12 +1,19 @@
-import {TypeOrmModuleOptions} from "@nestjs/typeorm";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeORMConfig : TypeOrmModuleOptions = {
+// 환경변수 로깅
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
+
+export const typeORMConfig: TypeOrmModuleOptions = {
     type: 'postgres',
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    port: parseInt(process.env.DB_PORT, 10),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: true,
 };
