b4270783ee320eb3833f27e4f038a23cc92b58ed
WOOIL, KIM <37162892+WinterLimited@users.noreply.github.com>
2024-02-19 02:12:59 +0900

    fix: proccess.env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_PASSWORD);
+  console.log(process.env.DB_PORT);
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
