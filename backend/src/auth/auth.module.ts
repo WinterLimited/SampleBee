07979678c85e7c00351efffea5515d73aa309de4
WOOIL, KIM <37162892+WinterLimited@users.noreply.github.com>
2024-02-19 02:00:44 +0900

    fix: userRepository Injectable

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import {JwtModule} from "@nestjs/jwt";
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import {PassportModule} from "@nestjs/passport";
 import {JwtStrategy} from "../common/jwt/jwt.strategy";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./user.entity";
 
 @Module({
   imports: [
@@ -17,6 +19,7 @@ import {JwtStrategy} from "../common/jwt/jwt.strategy";
           signOptions: { expiresIn: '1h' },
         }),
       }),
+      TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController, JwtStrategy],
   providers: [AuthService, PassportModule],
