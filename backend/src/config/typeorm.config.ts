d24a7cb6131867c2153a006f08153773a12eb341
WOOIL, KIM <37162892+WinterLimited@users.noreply.github.com>
2024-02-19 02:50:11 +0900

    fix: process.env Database

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeORMConfig : TypeOrmModuleOptions = {
     port: parseInt(process.env.DB_PORT, 10) || 5432,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     synchronize: true,
 };
