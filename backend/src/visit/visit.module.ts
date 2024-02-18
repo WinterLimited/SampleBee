476ba8bf89ba06185a2befc174f7bf2c558b792e
WOOIL, KIM <37162892+WinterLimited@users.noreply.github.com>
2024-02-19 01:58:58 +0900

    fix: visitRepository Injectable

--- backend/src/visit/visit.module.ts
+++ backend/src/visit/visit.module.ts
@@ -3,10 +3,13 @@ import { VisitController } from './visit.controller';
 import { VisitService } from './visit.service';
 import {PassportModule} from "@nestjs/passport";
 import {JwtStrategy} from "../common/jwt/jwt.strategy";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Visit} from "./visit.entity";
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forFeature([Visit]),
   ],
   controllers: [VisitController, JwtStrategy],
   providers: [VisitService]
